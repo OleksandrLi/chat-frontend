a2751a30642ff1abd923f07762ea04d005af0817
OleksandrLi <lipstort@gmail.com>
2023-09-28 17:14:09 +0300

    disable strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -12,15 +12,13 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <WebsocketProvider>
-          <AppRoutes />
-        </WebsocketProvider>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <WebsocketProvider>
+        <AppRoutes />
+      </WebsocketProvider>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
